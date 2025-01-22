5350cd46da7f7b406a9b1f83d24c07ccea83fcd3
BeTaJI <lilysh994@mail.ru>
2025-01-22 14:46:11 +0300

    разгрузил компонент аналитики сделал хардкодный конфиг для ChartItem

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,64 +1,14 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
-import { Chart, ChartTypeRegistry, registerables } from 'chart.js';
+import ChartItem from 'components/ChartItem';
 
-import { useTodoStore } from 'store/todoStore';
-
-import { filteredTasksHandler } from './helpers';
 import styles from './styles.module.scss';
 
 const Analytics: React.FC = () => {
   const [diagramType, setDiagramType] = useState('pie');
   const [filterType, setFilterType] = useState('День');
 
-  const [tasks] = useTodoStore((state) => [state.tasks]);
-
-  const chartRef = useRef<HTMLCanvasElement>(null);
-  const chartInstanceRef = useRef<Chart | null>(null); // для хранения экземпляра графика
-
-  const filteredTasks = useMemo(() => filteredTasksHandler(tasks, filterType), [tasks, filterType]);
-
-  useEffect(() => {
-    if (chartRef.current) {
-      const ctx = chartRef.current.getContext('2d');
-      if (ctx) {
-        Chart.register(...registerables);
-
-        // Если экземпляр графика уже есть - удалить его
-        if (chartInstanceRef.current) {
-          chartInstanceRef.current.destroy();
-        }
-
-        // новый график
-        chartInstanceRef.current = new Chart(ctx, {
-          type: diagramType as keyof ChartTypeRegistry,
-          data: {
-            labels: ['Done', 'Not done'],
-            datasets: [
-              {
-                label: 'Tasks',
-                data: [
-                  filteredTasks.filter((task) => task.isDone).length,
-                  filteredTasks.filter((task) => !task.isDone).length,
-                ],
-                backgroundColor: ['rgb(1, 254, 18)', 'rgba(250, 2, 2, 0.97)'],
-              },
-            ],
-          },
-          // options: {
-          //TODO  ... поиграться со св--вами натсройки канваса в конце
-          // },
-        });
-      }
-    }
-
-    // Очистка при размонтировании компонента удалять канвас
-    return () => {
-      if (chartInstanceRef.current) {
-        chartInstanceRef.current.destroy();
-      }
-    };
-  }, [tasks, diagramType, filteredTasks]);
+  const configChart = useMemo(() => ({ diagramType, filterType }), [diagramType, filterType]);
 
   return (
     <div className={styles.analyticsContainer}>
@@ -82,7 +32,7 @@ const Analytics: React.FC = () => {
           <option value='month'>Месяц</option>
         </select>
       </section>
-      <canvas ref={chartRef} />
+      <ChartItem config={configChart} />
     </div>
   );
 };
---
+++ src/components/ChartItem/index.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useMemo, useRef } from 'react';
+
+import { Chart, ChartTypeRegistry, registerables } from 'chart.js';
+
+import { filteredTasksHandler } from 'components/Analytics/helpers';
+
+import { useTodoStore } from 'store/todoStore';
+
+//! Переписать на нормальный конфиг который будет универсальным а не это
+interface ChartItemProps {
+  config: {
+    filterType: string;
+    diagramType: string;
+  };
+}
+
+const ChartItem = ({ config }: ChartItemProps) => {
+  const { filterType, diagramType } = config;
+
+  const chartRef = useRef<HTMLCanvasElement>(null);
+  const chartInstanceRef = useRef<Chart | null>(null); // для хранения экземпляра графика
+
+  const [tasks] = useTodoStore((state) => [state.tasks]);
+
+  const filteredTasks = useMemo(() => filteredTasksHandler(tasks, filterType), [tasks, filterType]);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext('2d');
+      if (ctx) {
+        Chart.register(...registerables);
+
+        // Если экземпляр графика уже есть - удалить его
+        if (chartInstanceRef.current) {
+          chartInstanceRef.current.destroy();
+        }
+
+        // новый график
+        chartInstanceRef.current = new Chart(ctx, {
+          type: diagramType as keyof ChartTypeRegistry,
+          data: {
+            labels: ['Done', 'Not done'],
+            datasets: [
+              {
+                label: 'Tasks',
+                data: [
+                  filteredTasks.filter((task) => task.isDone).length,
+                  filteredTasks.filter((task) => !task.isDone).length,
+                ],
+                backgroundColor: ['rgb(1, 254, 18)', 'rgba(250, 2, 2, 0.97)'],
+              },
+            ],
+          },
+          // options: {
+          //TODO  ... поиграться со св--вами натсройки канваса в конце
+          // },
+        });
+      }
+    }
+
+    // Очистка при размонтировании компонента удалять канвас
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+    };
+  }, [tasks, diagramType, filteredTasks]);
+
+  return (
+    <>
+      <canvas ref={chartRef} />
+    </>
+  );
+};
+
+export default ChartItem;
